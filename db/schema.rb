4317081ddfaa8a15eda081bff8d801e0edb86525
Beverly Lau <beverly.a.lau@gmail.com>
2016-01-23 21:39:08 -0800

    Add Location controller and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160124053552) do
+
+  create_table "locations", force: :cascade do |t|
+    t.float    "latitude",   limit: 24
+    t.float    "longitude",  limit: 24
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
 end
